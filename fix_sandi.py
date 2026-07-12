666e9222b584b1fd6c3d22d1afe599707a0b1ece
Misterios99 <misteriousas1@gmail.com>
2026-07-13 01:22:55 +0700

    Update fix_sandi.py

--- fix_sandi.py
+++ fix_sandi.py
@@ -1,4 +1,4 @@
 # Jalankan sekali, lalu hapus file ini
 with open("sandi.txt", "w", encoding="utf-8") as f:
-    f.write("P4522")
+    f.write("profit404")
 print("Sandi telah ditulis ulang dengan format bersih.")
