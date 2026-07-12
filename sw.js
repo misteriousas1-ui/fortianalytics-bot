a684c4c4a7781fa872efacbd0dbfb515cc5044dc
Misterios99 <misteriousas1@gmail.com>
2026-07-13 00:01:42 +0700

    Update fitur PWA
    Update: Tambah fitur PWA dan notifikasi

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'forti-terminal-v1';
+const ASSETS = [
+  '/',
+  '/index.html' // Pastikan nama file html Anda sesuai
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
